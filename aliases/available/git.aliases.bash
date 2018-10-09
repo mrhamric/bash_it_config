0736f8d2ecdfb5eb603ab2686c4e31c373f6936b
Matt Hamrick <mrhamric@gmail.com>
2018-10-08 19:08:14 -0600

    clean up git aliases

--- aliases/available/git.aliases.bash
+++ aliases/available/git.aliases.bash
@@ -2,59 +2,22 @@ cite 'about-alias'
 about-alias 'common git abbreviations'
 
 # Aliases
-alias gcl='git clone'
 alias ga='git add'
-alias gall='git add .'
-alias gus='git reset HEAD'
-alias gm="git merge"
 alias g='git'
 alias get='git'
 alias go='git checkout'
-alias gst='git status'
 alias gs='git status'
 alias gss='git stash -u'
 alias gsp='git stash pop'
-alias gl='git pull'
 alias gpr='git pull --rebase'
-alias gpp='git pull && git push'
-alias gup='git fetch && git rebase'
-alias gpo='git push origin'
-alias gdv='git diff -w "$@" | vim -R -'
 alias gc='git commit -v'
-alias gca='git commit -v -a'
-alias gcm='git commit -v -m'
-alias gci='git commit --interactive'
+alias gcm='git commit  -v -m'
 alias gb='git branch'
-alias gba='git branch -a'
 alias gcount='git shortlog -sn'
-alias gcp='git cherry-pick'
-alias gco='git checkout'
-alias gexport='git archive --format zip --output'
 alias gdel='git branch -D'
-alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
-alias gll='git log --graph --pretty=oneline --abbrev-commit'
-alias gg="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias ggs="gg --stat"
-alias gsl="git shortlog -sn"
-alias gw="git whatchanged"
-alias gt="git tag"
-alias gta="git tag -a"
-alias gtd="git tag -d"
-alias gtl="git tag -l"
 alias gra='git commit --amend --no-edit'
-alias undeployed="git rev-list $(curl -sI www.statuspage.io | grep -i X-StatusPage-Version | sed -e 's/X-StatusPage-Version: //' | tr -d '\r\n' | awk '{print $1"..HEAD"}')"
-
-# StatusPage
-alias rc='foreman run bin/rails c'
-alias rdb='foreman run bin/rails db'
-alias rpt='foreman run rake parallel:test[5]'
-alias tdl='tail -5000f log/development.log'
-alias ttl='tail -5000f log/test.log'
-alias cap='bundle exec cap'
-alias ss='bin/spring stop'
-alias tst='bundle exec ruby -Itest'
-alias es="/Users/matt/Documents/elasticsearch-2.3.2/bin/elasticsearch ; exit;"
-
+alias catchup='git pull upstream master && git push origin master'
+alias cu='git pull upstream master && git push origin master'
 
 case $OSTYPE in
   darwin*)
